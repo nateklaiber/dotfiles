b3316477f356970e5a6ac1184f41e8d7797581f1
Nate Klaiber <nate@theklaibers.com>
2011-01-15 13:15:46 -0500

    adding some extra shell scripts for quick tasks

---
+++ scripts/w3-passenger.sh
@@ -0,0 +1,1 @@
+rvmsudo /Users/nateklaiber/.rvm/gems/ruby-1.8.7-p302@w3-public/gems/passenger-3.0.0/bin/passenger-memory-stats
---
+++ scripts/w3_start_starling.sh
@@ -0,0 +1,1 @@
+starling -d -P /pub/www/within3_development/starling/starling.development.pid -q /pub/www/within3_development/starling/ -p 15151
