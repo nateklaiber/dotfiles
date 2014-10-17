bd1dd86fab7a8217d23cdf6fca15fd647002d273
Nate Klaiber <nate@theklaibers.com>
2014-10-17 16:00:16 -0400

    Remove W3 scripts

--- scripts/w3-passenger.sh
+++
@@ -1,1 +0,0 @@
-rvmsudo /Users/nateklaiber/.rvm/gems/ruby-1.8.7-p302@w3-public/gems/passenger-3.0.0/bin/passenger-memory-stats
--- scripts/w3_start_starling.sh
+++
@@ -1,1 +0,0 @@
-starling -d -P /pub/www/within3_development/starling/starling.development.pid -q /pub/www/within3_development/starling/ -p 15151
